ac96f38ed9790a675acc37f80c4d3c416f750839
aljawaid <jawaid_imran@hotmail.com>
2022-10-26 12:28:25 +0100

    UPDATE: Translation files
    - Added template for German
    - Fixed grammar for `en_GB`
    - Preparing to fix #1

--- Locale/en_GB/translations.php
+++ Locale/en_GB/translations.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-  'Agile Indicators displays the measurements for Task Priorities and Task Complexities in an easy to use general scale. Task Priorities are scaled between P1 and P5 whilst Task Complexities are scored as 0-50.' => 'Agile Indicators displays the measurements for Task Priorities and Task Complexities in an easy to use general scale. Task Priorities are scaled between P1 and P5 whilst Task Complexities are scored as 0-50.',
+  'Agile Indicators displays the measurements for Task Priorities and Task Complexities in an easy to use general scale. Task Priorities are scaled between P1 and P5 whilst Task Complexities are scored as 0-50.' => 'Agile Indicators display the measurements for Task Priorities and Task Complexities in an easy to use general scale. Task Priorities are scaled between P1 and P5 whilst Task Complexities are scored as 0-50.',
   'Task Priorities' => 'Task Priorities',
   'P1' => 'P1',
   'P2' => 'P2',
