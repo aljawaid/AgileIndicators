01df45b45a60aaf1ae6697473f3d0546e9a0cec1
aljawaid <jawaid_imran@hotmail.com>
2022-11-10 09:39:48 +0000

    FIX: Translations

--- Plugin.php
+++ Plugin.php
@@ -30,7 +30,7 @@ class Plugin extends Base
 
     public function getPluginDescription()
     {
-        return t('Agile Indicators displays the measurements for Task Priorities and Task Complexities in an easy to use general scale. Task Priorities are scaled between P1 and P5 whilst Task Complexities are scored as 0-50.');
+        return t('AgileIndicators displays the measurements for Task Priorities and Task Complexities in an easy to use general scale. Task Priorities are scaled between P1 and P5 whilst Task Complexities are scored as 0-50.');
     }
 
     public function getPluginAuthor()
